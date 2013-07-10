374381cf629ba2c7f8efc74486c8e29bfd798b63
Antti Rasmus <antti.rasmus@gmail.com>
2013-07-10 19:41:28 +0300

    Don't run chsh, instruct user instead
    Due to PAM auth problems when piping to sh

--- install.sh
+++ install.sh
@@ -7,4 +7,4 @@ find $ENVDIR -type f -name .\* -exec ln -s {} $HOME \;
 # Setup zsh
 d=~/.oh-my-zsh
 test -d $d || git clone git://github.com/robbyrussell/oh-my-zsh.git $d
-chsh -s /bin/zsh
+test $SHELL = /bin/zsh || echo -e "To make zsh your default shell, run:\n    chsh -s /bin/zsh"
